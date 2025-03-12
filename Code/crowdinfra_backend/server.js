e655e75b7519ca56968bf51b9a900c5c76f49866
Mayank Mishra <mrmishramayank2005@gmail.com>
2025-03-13 00:13:55 +0530

    backend

---
+++ Code/crowdinfra_backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/auth", require("./routes/auth"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
